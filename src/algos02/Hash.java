0d03b51f2211870c1586da2b079fe3660ea1288e
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-04-06 22:23:11 +0800

    first

---
+++ src/algos02/Hash.java
@@ -0,0 +1,23 @@
+/**
+ * Simple Hash functions (Finn. hajautus)
+ */
+package algos02;
+
+/**
+ * @author Antiik.dev
+ * @version 6 April 2022
+ */
+public class Hash {
+
+	
+	
+	/**
+	 * Main for testing
+	 * @param args not in use
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
