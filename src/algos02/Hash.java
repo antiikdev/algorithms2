6eeb93b0b4d3cfdfb7eac21f7decbfee2a93e555
antiikdev <ilkka.a.kotilainen@gmail.com>
2022-04-11 15:43:08 +0800

    hash open addressing

--- src/algos02/Hash.java
+++ src/algos02/Hash.java
@@ -1,5 +1,7 @@
 /**
- * Simple Hash functions (Finn. hajautus), where
+ * Simple hash open addressing for positive integer numbers
+ * Finn. hajautus positiivisille kokonaisluvuille
+ * avoimella osoitteenmuodostuksella
  * [0] = free (VAPAA)
  * [-1] = deleted (POISTETTU)
  */
@@ -13,9 +15,9 @@ import java.util.Arrays;
  */
 public class Hash {
 	
-	public int m = 20;				// array size (m >= n)
-	public int n = 0;				// amount of numbers
-	public int[] t = new int[m];	// hash table
+	public int m = 6;				// array max size (m >= n)
+	public int n = 0;				// amount of numbers saved in the array
+	public int[] t = new int[m];	// hash table with max (m)
 
 	
 	/**
@@ -30,39 +32,72 @@ public class Hash {
 	
 	
 	/**
-	 * Hash function with open address creation
-	 * (hajautusfunktio avoimella osoitteenmuodostuksella)
-	 * @param k number to same index
-	 * TODO POSSIBLE: yksikasitteisyys; is k already saved
+	 * ADD Hash function with open address creation
+	 * Finn. LISAA hajautusfunktio avoimella osoitteenmuodostuksella
+	 * @param k added number
 	 */
-	private void h(int k) {
+	private void add(int k) {
 		if ( this.n > this.m ) return;	// 1. m >= n
-		if ( t[k] == 0 ) {				// 2. first index h(k)
-			t[k] = k;
-			n++;
-			return;
-		}
-		for (int i = k+1; i < m; i++) {	// 3. h(k,i), i=0,1,2,...,
-			if ( t[i] == 0 ) {
-				t[i] = k;
+		
+		for (int i = 0; i < m; i++) {	// 2. h(k,i), i=0,1,2,...,
+			int hash = hLinear(k, i);
+			if ( t[hash] == 0 ) {
+				t[hash] = k;
 				n++;
 				break;
 			}
-		}								// if not found, not saved
+		}							
 	}
 	
 	
 	/**
-	 * @return Hash table to string
+	 * Hash linear search
+	 * @param key being searched
+	 * @return index of the number if found, -1 if not found
+	 */
+	public int search(int key) {
+		int index = -1;
+		
+		for (int i = 0; i < this.m; i++) {
+			int hash = hLinear(key, i);
+			if ( this.t[hash] == key ) return hash;
+			// If free slot 0, then key does not exist:
+			if ( this.t[hash] == 0 ) break;
+		}
+		return index;
+	}
+	
+	
+	/**
+	 * Delete index from hash table
+	 * If found key is found
+	 * @param key being deleted¨
+	 */
+	public void delete(int key) {
+		int search = search(key);
+		if (search >= 0 ) {
+			this.t[search] = -1;
+			n--;
+		}
+	}
+	
+	/**
+	 * Linear search
+	 * @param key hash
+	 * @param i number
+	 * @return key+i mod m
+	 */
+	public int hLinear(int key, int i) {
+		return (key + i) % this.m;
+	}
+	
+	
+	/**
+	 * @return Prints hash array to string
 	 */
 	public void print() {
 		System.out.println(Arrays.toString(t));
 	}
-	
-	
-	// TODO: Search (etsi)
-	
-	// TODO: Delete (poista)
 	
 	
 	/**
@@ -70,13 +105,34 @@ public class Hash {
 	 * @param args not in use
 	 */
 	public static void main(String[] args) {
+		// New hash class
 		Hash haja = new Hash();
 		
-		System.out.println("Hash array before add:");
+		// Testing of empty hash array and print function
+		System.out.print("Hash array before: ");
 		haja.print();
 		
-		haja.h(1);
-		System.out.println("Hash array after add:");
+		// Testing of hash add function 
+		haja.add(1);
+		haja.add(1);
+		haja.add(2);
+		System.out.print("Hash array after:  ");
+		haja.print();
+		
+		// Testing search function
+		int search = 2;
+		int result = haja.search(2);
+		System.out.println("Index of the number " + search +
+				" (-1 if not found): " + result);
+		search = 6;
+		result = haja.search(7);
+		System.out.println("Index of the number " + search +
+				" (-1 if not found): " + result);
+		
+		// Testing delete function
+		int del = 2;
+		haja.delete(del);
+		System.out.print("Number " + del + " deleted:  ");
 		haja.print();
 	}
 
